974430143b4c3b95ad4bfd9325bd8a0ca453bdda
yan55 <773953000@qq.com>
2017-11-05 09:42:54 +0800

    创建初始化

---
+++ example/src/main/java/com/example/yandanfeng/fastyandanfeng/hah.java
@@ -0,0 +1,20 @@
+package com.example.yandanfeng.fastyandanfeng;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017-11-05
+ */
+
+public class hah extends AppCompatActivity {
+
+    /*
+    * hehe
+    *
+    * */
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
